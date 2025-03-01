a280bc0643f31caecee2a87d56a2716f5d689abb
utsav-kumar7856891309 <15102004utsavkumar@gmail.com>
2025-03-01 22:56:20 +0530

    clock

--- script5.js
+++ script5.js
@@ -10,28 +10,34 @@
 //     const randomIndex = Math.floor(Math.random() * quotes.length);
 //     document.getElementById('quote').textContent = quotes[randomIndex];
 // }
-function calculate(operation){
-    const num1=parseInt(document.getElementById('num1').value);
-    const num2=parseFloat(document.getElementById('num2').value);
-    const result=document.getElementById('result');
-    if(isNaN(num1)|| isNaN(num2)){
-        result.textContent="Please enter valid numbers!";
-        return;
-    }
-    let output;
-    switch(operation){
-        case 'add':
-            output=num1+num2;
-            break;
-        case 'subtract':
-            output=num1-num2;
-            break;
-        case 'multiply':
-            output=num1*num2;
-            break;
-        case divide:
-            output = num2 !== 0 ? num1 / num2 : "Cannot divide by zero!";
-            break;
-    }
-    result.textContent = "Result: " + output;
+// function calculate(operation){
+//     const num1=parseInt(document.getElementById('num1').value);
+//     const num2=parseFloat(document.getElementById('num2').value);
+//     const result=document.getElementById('result');
+//     if(isNaN(num1)|| isNaN(num2)){
+//         result.textContent="Please enter valid numbers!";
+//         return;
+//     }
+//     let output;
+//     switch(operation){
+//         case 'add':
+//             output=num1+num2;
+//             break;
+//         case 'subtract':
+//             output=num1-num2;
+//             break;
+//         case 'multiply':
+//             output=num1*num2;
+//             break;
+//         case divide:
+//             output = num2 !== 0 ? num1 / num2 : "Cannot divide by zero!";
+//             break;
+//     }
+//     result.textContent = "Result: " + output;
+// }
+function updateclcok(){
+    const now= new Date();
+    const time=now.toLocaleTimeString();
+    document.getElementById('clock').textContent=time;
 }
+setInterval(updateclcok,1000);
